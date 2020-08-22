7665b232fdac3fcf2c499f89097ac852e41c76ca
unknown <you@example.com>
2020-08-22 15:40:05 +0300

    create new projct and install search filter pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng2-search-filter-demo';
+
+  term: string;
+
+  filterData = [
+    {
+      firstName: 'Celestine',
+      lastName: 'Schimmel',
+      address: '7687 Jadon Port'
+    },
+    {
+      firstName: 'Johan',
+      lastName: 'Ziemann PhD',
+      address: '156 Streich Ports'
+    },
+    {
+      firstName: 'Lizzie',
+      lastName: 'Schumm',
+      address: '5203 Jordon Center'
+    },
+    {
+      firstName: 'Gavin',
+      lastName: 'Leannon',
+      address: '91057 Davion Club'
+    },
+    {
+      firstName: 'Lucious',
+      lastName: 'Leuschke',
+      address: '16288 Reichel Harbor'
+    }
+  ]
 }
+
+
